7eb42e16868c760abc0e5ee802267e5fa47d6601
Diego Polverelli <diegopolverelli@hotmail.com>
2025-04-09 21:59:32 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const connection = mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("./src/public"))
 
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
